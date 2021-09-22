5965bfac36be24f4a6f0c6db8e783064d22e4645
谢瑾珩 <577870940@qq.com>
2021-09-22 21:00:47 +0800

    update

---
+++ monitoring.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+file=file.txt
+if [ ! -f "$file"  ]
+then
+    echo "File.txt not present. Exiting Script."
+    exit
+fi
+#Set date variable
+date=$(date)
+
+#Get IPS from alerts file
+grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' file.txt > ips_temp
+
+#Remove duplicates from file and pipe into Ips.txt
+sort -u ips_temp > ips.txt
+
+#Start Forloop to ping listed IPs in the ips.txt file.
+grep -v '^ *#' < ips.txt | while IFS= read -r ip
+do
+    ping -c 1 "$ip" > /dev/null
+    if [ $? -eq 0 ]; 
+    then
+    echo "$date" "Node" "$ip" "is up"
+    else
+    echo "$date" "Node" "$ip" "down"
+    fi
+done
+#End of Forloop
+#Remove files that were used.
+rm ips_temp
+rm ips.txt
